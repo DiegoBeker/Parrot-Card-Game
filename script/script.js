0c8fe8f5fa9d03c08b0f32eb0a16cd2a278757a5
Diego Beker <diegobecker2007@hotmail.com>
2023-01-13 09:45:40 -0300

    Reiniciar partida adicionado

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 const list = document.querySelector('.deck');
-let gifs = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+const gifs = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 let cards = prompt('Numero de cartas par Min = 4 Max = 14');
 let deck = [];
 let move = [];
@@ -10,11 +10,12 @@ buildDeck();
 insertDeckOnHtml();
 
 function cardsNumber() {
-    if (cards % 2 !== 0 || cards < 4 || cards > 14)
+    const condition = cards % 2 !== 0 || cards < 4 || cards > 14;
+    if (condition)
         alert('Número inválido');
-    while (cards % 2 !== 0 || cards < 4 || cards > 14) {
+    while (condition) {
         cards = prompt('Numero de cartas par Min = 4 Max = 14');
-        if (cards % 2 !== 0 || cards < 4 || cards > 14)
+        if (condition)
             alert('Número inválido');
     }
 }
@@ -47,20 +48,20 @@ function comparator() {
 
 let checking = false;
 function select(card) {
-    if (!card.classList.contains('found')&&checking==false) {
+    if (!card.classList.contains('found') && checking == false) {
         move.push(card);
         card.querySelector('.front-face').classList.add('open');
         card.querySelector('.back-face').classList.add('open');
         moves++;
         if (move.length === 2) {
-			checking = true;
+            checking = true;
             const img0 = move[0].querySelector('.back-face img').getAttribute('src');
             const img1 = move[1].querySelector('.back-face img').getAttribute('src');
             if (img0 == img1) {
                 move[0].classList.add('found');
                 move[1].classList.add('found');
                 move = [];
-				checking = false;
+                checking = false;
             } else {
                 setTimeout(() => {
                     move[0].querySelector('.front-face').classList.remove('open');
@@ -69,7 +70,7 @@ function select(card) {
                     move[1].querySelector('.back-face').classList.remove('open');
                     move = [];
                 }, 1000);
-                setTimeout(()=>{checking = false;},1000);	
+                setTimeout(() => { checking = false; }, 1000);
             }
             endGameCheck();
         }
@@ -77,6 +78,27 @@ function select(card) {
 }
 
 function endGameCheck() {
-    if (document.querySelectorAll('.found').length === Number(cards))
-        setTimeout(alert, 1000, `Você ganhou em ${moves} jogadas!`);
+    if (document.querySelectorAll('.found').length === Number(cards)) {
+        alert(`Você ganhou em ${moves} jogadas! A duração do jogo foi de Y segundos!`);
+        let restart = prompt('Deseja reiniciar a partida("sim" ou "não"');
+        while (restart !== 'não') {
+            if (restart === 'sim') {
+                resetGame();
+                cardsNumber();
+                buildDeck();
+                insertDeckOnHtml();
+                break;
+            } else {
+                restart = prompt('Deseja reiniciar a partida("sim" ou "não"');
+            }
+        }
+    }
+}
+
+function resetGame() {
+    deck = [];
+    move = [];
+    moves = 0;
+    list.innerHTML = "";
+    cards = prompt('Numero de cartas par Min = 4 Max = 14');
 }
